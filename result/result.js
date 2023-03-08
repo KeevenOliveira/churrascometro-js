69bc93e8b90b1fc2a4ec9a004a0b659a05022a24
Keeven Oliveira <keevinhuholiveira@gmail.com>
2023-03-08 13:10:59 -0300

    [FIX]: Solve problem with button calculate

--- result/result.js
+++ result/result.js
@@ -1,96 +1,79 @@
 // Carne - 400gr por pessoa + de 6horas - 650
 // Cerveja - 1200ml por Pessoa + 6 horas - 2000ml
 // Refrigerante/água - 1000ml por pessoa + 6 horas 1500ml
-// Suco - 600ml por pessoa 
+// Suco - 600ml por pessoa
 
 // Crianças valem por 0,5
 
-var button = document.querySelector('.button');
+var button = document.querySelector(".button");
 
 const inputAdultos = document.getElementById("adultos");
 const inputCriancas = document.getElementById("criancas");
 const inputDuracao = document.getElementById("duracao");
 
-const resultado = document.getElementById("result")
+const resultado = document.getElementById("result");
 
 function carnePP(duracao) {
-  
-  if(duracao >= 6) {
+  if (duracao >= 6) {
     return 650;
-  }else{
+  } else {
     return 400;
   }
 }
 
 function sucoPP(duracao) {
-  
-  if(duracao >= 6) {
+  if (duracao >= 6) {
     return 1000;
-  }else{
+  } else {
     return 600;
   }
 }
 
 function bebidasPP(duracao) {
-  
-  if(duracao >= 6) {
+  if (duracao >= 6) {
     return 1000;
-  }else{
+  } else {
     return 600;
   }
 }
 
-// var i = 0;
-
-// button.addEventListener('click', function click(e){
-//   i++;
-//   console.log('Esse botão foi chamado %d vez', i);
-// })
-
-var clicked = false;
+let clicked = false;
 
 function calcular() {
+  if (!clicked) {
+    const adultos = inputAdultos.value;
+    const criancas = inputCriancas.value;
+    const duracao = inputDuracao.value;
 
-  if(!clicked){
-    console.log('First click')
+    const qdtTotalCarne =
+      carnePP(duracao) * adultos + (carnePP(duracao) / 2) * criancas;
+    const qdtTotalSuco = sucoPP(duracao) * adultos;
+    const qdtTotalBebidas =
+      bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2) * criancas;
 
-  const adultos = inputAdultos.value;
-  const criancas = inputCriancas.value;
-  const duracao = inputDuracao.value;
-
-  const qdtTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2 * criancas);
-  const qdtTotalSuco = sucoPP(duracao) * adultos;
-  const qdtTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2 * criancas);
-
-  if(adultos == ""|| criancas == "" || duracao == ""){
-    return resultado.innerHTML =`<h2 class="result-info ult">Informação incompleta<h2>`
-  }
-  //coloca-se o "+" após o sinal de "=" para deixar entendido que preciso imprimir mais de um innerHTML
-  resultado.innerHTML = `<h2 class="result-info">Você vai precisar de:</h2>`
-  resultado.innerHTML += `
+    if (!adultos || !criancas || !duracao) {
+      return (resultado.innerHTML = `<h2 class="result-info ult">Informação incompleta<h2>`);
+    }
+    //coloca-se o "+" após o sinal de "=" para deixar entendido que preciso imprimir mais de um innerHTML
+    resultado.innerHTML = `<h2 class="result-info">Você vai precisar de:</h2>`;
+    resultado.innerHTML += `
     <div class="result-block">
       <img class="img" src="./assets/meal.svg"/>
-      <p>${qdtTotalCarne/1000} Kg de Carne</p>
+      <p>${qdtTotalCarne / 1000} Kg de Carne</p>
     </div>
-  `
-  resultado.innerHTML += `
+  `;
+    resultado.innerHTML += `
     <div class="result-block">
       <img class="img" src="./assets/juice.svg"/>
-      <p>${Math.ceil(qdtTotalSuco/1500)} Litro(s) de Suco</p>
+      <p>${Math.ceil(qdtTotalSuco / 1500)} Litro(s) de Suco</p>
     </div>
-  `
-  resultado.innerHTML += `
+  `;
+    resultado.innerHTML += `
     <div class="result-block ult">
       <img class="img" src="./assets/refri.svg"/>
-      <p>${Math.ceil(qdtTotalBebidas/2000)} Litro(s) de Refrigerante</p>
+      <p>${Math.ceil(qdtTotalBebidas / 2000)} Litro(s) de Refrigerante</p>
     </div>
-  `
+  `;
     clicked = false;
-  } else{
-    console.log('Clicked once more');
   }
-  // button.addEventListener('click', function click(e){
-  //   i++;
-  //   console.log('Esse botão foi chamado %d vez', i);
-  // })
 }
